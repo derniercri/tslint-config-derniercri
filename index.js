7655527b3ece2a4896cbc3f428712726ae7a7184
Jean-Serge Monbailly <jean-serge@derniercri.io>
2019-02-22 15:00:37 +0100

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'extends': [
+    'tslint:recommended',
+    'tslint-config-standard',
+    'tslint-react',
+    'tslint-config-prettier'
+  ],
+  'rules': {
+    'jsx-boolean-value': false,
+    'jsx-no-lambda': false,
+    'newline-before-return': true
+  }
+}
